907825310c4309241c0214779fb2262c68c907e0
Good-one33 <81611086+Good-one33@users.noreply.github.com>
2021-06-07 23:11:21 +0200

    update

---
+++ cards.js
@@ -0,0 +1,13 @@
+
+
+// animation de beinvenue
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("container"),
+  path: "welcome.json",
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  name: "Demo ",
+});
+
+
