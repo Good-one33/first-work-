d1d72d01d691b806cbd06a8547a2bbb69f16bbc5
Malik <benali.abdelmalek63@gmail.com>
2021-12-21 22:05:19 +0100

    now it's a clean repo

---
+++ JS/welcomeAnimation.js
@@ -0,0 +1,13 @@
+
+
+// animation de beinvenue
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("container"),
+  path: "Bodymovin/welcomeAnimation.json",
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  name: "Demo ",
+});
+
+
