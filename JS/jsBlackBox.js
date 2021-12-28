7c5563e42d89647d0e6e94bbf905173a35c3bec1
Malik <benali.abdelmalek63@gmail.com>
2021-12-28 16:24:51 +0100

    moved get ready to js

--- JS/jsBlackBox.js
+++ JS/jsBlackBox.js
@@ -4,10 +4,17 @@
         document.getElementById("demo").innerHTML = Date();
         document.getElementById("demo2").style.color = "red";
       }
-   
-// test function 
+
  function myFunction(){
     document.getElementById("demo2").style.color = "blue";
  }
      
  
+
+ // don't forget the ready fucnciton !!
+ $(document).ready(function(){
+   $(".button").click(function(){
+     let i = $(this).attr('id');            
+     $("#displayContent").html($("#pc"+i).html());    
+   });        
+});
