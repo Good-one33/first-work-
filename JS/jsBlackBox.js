bcfd40f1c7289bbc45a9539faaf6bd49f79a25d5
Malik <benali.abdelmalek63@gmail.com>
2021-12-28 15:31:47 +0100

    added buttons to see if evry thing okay

--- JS/jsBlackBox.js
+++ JS/jsBlackBox.js
@@ -1,5 +1,13 @@
 
     
+     function displayDate() {
+        document.getElementById("demo").innerHTML = Date();
+        document.getElementById("demo2").style.color = "red";
+      }
    
 // test function 
+ function myFunction(){
+    document.getElementById("demo2").style.color = "blue";
+ }
+     
  
