1a8f4f8557c64e3ab481b6a13a184f0e107e3d1b
Malik <benali.abdelmalek63@gmail.com>
2021-12-28 15:14:24 +0100

    the code for boxes in html header

--- JS/jsBlackBox.js
+++ JS/jsBlackBox.js
@@ -1,11 +1,5 @@
 
     
-      $(document).ready(function(){
-          $(".button").click(function(){
-            let i = $(this).attr('id');            
-            $("#displayContent").html($("#pc"+i).html());    
-          });        
-      });
    
 // test function 
  
